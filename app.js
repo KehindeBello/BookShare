71182f26a8174f84b808a0f5f80094e923de24e9
KehindeBello <adedotunomomeji@gmail.com>
2023-09-15 17:51:51 +0100

    add: db connection,log-streams,route(book,about,404)

--- app.js
+++ app.js
@@ -2,20 +2,39 @@ import express from "express";
 import morgan from "morgan";
 import fs from "fs"
 import path from "path";
+import { Book } from "./models/Books.js";
+import mongoose from "mongoose";
+import { config } from "./config.js"
+import { Bookrouter } from "./routes/booksRoutes.js";
 
 const port = 3300 | process.env.port;
-const app = express()
 const root = "./"
+const app = express()
 
+// connect to db and start server
+await mongoose.connect(config.MONGO_URI,  { useNewUrlParser: true})
+    .then((connection) => console.log("Connected to db"))
+    .then(() => app.listen(port))
+    .then(() => console.log(`Listening on http://localhost:${port}`))
+    .catch((error) => {console.log(`Error - ${error}`)})
+
+
+// streams for logs 
 const writestream = fs.createWriteStream(path.join(root, "access.log"), {flags:"a"})
-
 app.use(morgan(":date - :method - :url - :status - :response-time ms", {stream: writestream}));
 
-app.get('/', (req, res) => {
-    res.send("Flaunt and discover literary gems");
+// parser middleware
+app.use(express.json());
+
+//Book routes
+app.use('/books', Bookrouter);
+
+// About Route
+app.get('/about', (req,res) => {
+    res.sendFile("./about.html", {root: './views'});
 })
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-    console.log(`Ctrl + C to cancel`);
+// 404 page
+app.use((req,res) => {
+    res.status(404).send("Page does not exist");
 })
