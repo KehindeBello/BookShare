e86fb1a7dfa69403b268868f760f4969fa850091
KehindeBello <adedotunomomeji@gmail.com>
2023-09-15 17:45:58 +0100

    add: Book Schema

---
+++ models/Books.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    published_date : {
+        type: Date,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    }
+
+}, {timestamps: true})
+
+export const Book = mongoose.model("book", bookSchema);
