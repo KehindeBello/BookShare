460c582d9e2d07ee506806722cfbaa4efd6b2967
KehindeBello <adedotunomomeji@gmail.com>
2023-09-16 11:33:07 +0100

    add: get,delete book routes

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -5,6 +5,8 @@ const router = Router();
 const bookController = new BookController();
 
 router.get('/', bookController.all_books);
-router.post('/', bookController.add_book)
+router.post('/', bookController.add_book);
+router.get('/:id', bookController.get_book);
+router.delete('/:id',bookController.delete_book);
 
 export const Bookrouter = router;
