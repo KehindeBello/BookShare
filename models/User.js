f08f35612cd2b7e761bce6d7fb85176d86dd088d
KehindeBello <adedotunomomeji@gmail.com>
2023-09-27 15:18:20 +0100

    add: mongoose hook to hash password

--- models/User.js
+++ models/User.js
@@ -1,19 +1,29 @@
 import mongoose from "mongoose";
+import isEmail from "validator/lib/isEmail.js";
+import { hashPassword } from "../controllers/user.service.js";
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        validate: [isEmail, "Enter a valid email"]
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: [6, "Minimum password length is 6 characters"]
+    },
+    username: {
+        type: String,
+        unique: true
     }
 }, {timestamps: true});
+
+userSchema.pre('save', async function(){
+    this.password = await hashPassword(this.password);
+})
 
 export const User = mongoose.model("user", userSchema);
