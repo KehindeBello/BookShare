12e697b505aa25396163556c3d537849fb375dda
KehindeBello <adedotunomomeji@gmail.com>
2023-09-27 16:45:13 +0100

    implement: dotenv for env variables

--- config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    PORT: process.env.PORT || 3300,
-    MONGO_URI: "mongodb://127.0.0.1:27017/BookShare",
-    JWT: {
-        maxAge: 86400,
-        secret: "qwertyuiopasdfghjkl"
-    }
-}
-
