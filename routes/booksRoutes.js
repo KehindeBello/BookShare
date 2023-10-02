01c9d2b8416b514cf0863ed0a3b3ba98cc97a44e
KehindeBello <adedotunomomeji@gmail.com>
2023-10-02 01:13:01 +0100

    protect book routes

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { BookController } from '../controllers/book.controller.js';
+import { requireAuth } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 const bookController = new BookController();
 
-router.get('/', bookController.all_books);
-router.post('/', bookController.add_book);
-router.get('/:id', bookController.get_book);
-router.delete('/:id',bookController.delete_book);
+router.get('/', requireAuth ,bookController.all_books);
+router.post('/', requireAuth ,bookController.add_book);
+router.get('/:id', requireAuth ,bookController.get_book);
+router.delete('/:id', requireAuth ,bookController.delete_book);
 
 export const Bookrouter = router;
