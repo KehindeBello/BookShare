83c88f1a5a8be3fdc9729cd40fb7a6e60d42cc45
KehindeBello <adedotunomomeji@gmail.com>
2023-10-02 01:08:06 +0100

    added logout route

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,9 +4,8 @@ import { UserController } from "../controllers/user.controller.js"
 const router = Router()
 const userController = new UserController()
 
-router.get('/signup', userController.get_signup);
 router.post('/signup', userController.post_signup);
-router.get('/login', userController.get_login);
 router.post('/login', userController.post_login);
+router.post('/logout', userController.logout);
 
 export const UserRouter = router;
