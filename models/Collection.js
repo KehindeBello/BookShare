8040818fe00ff44c64def490be757a598b8e62aa
KehindeBello <adedotunomomeji@gmail.com>
2023-10-02 01:20:16 +0100

    modify: user_id field not an array

--- models/Collection.js
+++ models/Collection.js
@@ -1,20 +1,21 @@
 import mongoose, { SchemaType } from "mongoose";
-import { User } from "./User";
+import { User } from "./User.js";
 const Schema = mongoose.Schema;
 
 const collectionSchema = new Schema({
     user_id: {
-        type: User._id,
-        required: true
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
     },
     name: {
         type: String,
         required: true
     },
-    books: [{
+    books: {
         type: [Schema.Types.ObjectId],
         ref: 'Book'
-    }]
+    }
 }, {timestamps: true});
 
-export const Collection = mongoose.model("collectionModel", collectionSchema);
+export const Collection = mongoose.model("collection", collectionSchema);
