d7a9c8b3bd67628a48c202a94051d3910ae681bd
KehindeBello <adedotunomomeji@gmail.com>
2023-10-02 01:23:41 +0100

    added verify token and loggedIn user function

--- controllers/user.service.js
+++ controllers/user.service.js
@@ -38,3 +38,14 @@ export const createToken = async (data) => {
 export const comparePassword = async (password, hashedPassword) => {
     return bcrypt.compare(password, hashedPassword);
 }
+
+// verify jwt token
+export function verifyToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET)
+}
+
+// get id of logged in user
+export function loggedInUser(token) {
+    const decodedToken = verifyToken(token);
+    return decodedToken.data
+}
