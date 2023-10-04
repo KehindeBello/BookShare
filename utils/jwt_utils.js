d093311b6cb8d353782e46166ed4ea58d96598f4
KehindeBello <adedotunomomeji@gmail.com>
2023-10-04 22:44:43 +0100

    separated jwt utility functions

---
+++ utils/jwt_utils.js
@@ -0,0 +1,17 @@
+// create jwt token
+export const createToken = async (data) => {
+    return jwt.sign({data}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_MAXAGE
+    })
+}
+
+// verify jwt token
+export function verifyToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET)
+}
+
+// get id of logged in user
+export function loggedInUser(token) {
+    const decodedToken = verifyToken(token);
+    return decodedToken.data
+}
