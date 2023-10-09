91697b9488820e8d9a40fa0287e80d0b28b9e9df
KehindeBello <adedotunomomeji@gmail.com>
2023-10-09 10:02:43 +0100

    added route to add books to collection

--- routes/collectionsRoutes.js
+++ routes/collectionsRoutes.js
@@ -7,6 +7,6 @@ const collectionConroller = new CollectionController()
 
 router.post('/', requireAuth, collectionConroller.create_collection)
 router.get('/', requireAuth, collectionConroller.get_collection);
-router.post('/:id', requireAuth, collectionConroller.add_book_to_collection);
+router.put('/:id', requireAuth, collectionConroller.books_to_collection);
 
 export const Collectionrouter = router;
