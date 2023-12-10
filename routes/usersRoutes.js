636e6064c5947b5c487b34c7b72109a280aafc7f
kehindebello <adedotunomomeji@gmail.com>
2023-12-10 10:52:30 +0100

    add: route to collect username

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { UserController } from "../controllers/user.controller.js"
+import { requireAuth } from "../middlewares/authMiddleware.js";
 
 const router = Router()
 const userController = new UserController()
 
 router.post('/signup', userController.post_signup);
+router.post('/username', requireAuth, userController.post_username);
 router.post('/login', userController.post_login);
 router.post('/logout', userController.logout);
 
