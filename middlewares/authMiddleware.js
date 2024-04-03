2b2c69dc712378d66bb151f2db85cbb51fa26c08
kehindebello <adedotunomomeji@gmail.com>
2024-04-03 13:22:47 +0100

    refactor: check and verify token in headers

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,24 +2,15 @@
 import { verifyToken } from "../utils/jwt_utils.js";
 
 export const requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt;
-    if (token) {
-        try {
-            verifyToken(token)
-            next();
-        } catch (error) {
-            console.log(error.message);
-            return res.status(400).json({
-                message: "Unauthorized",
-                status: false,
-                data: null
-            })
-        }
-    } else {
-        return res.status(400).json({
-            message: "Unauthorized",
-            status: false,
-            data: null
-        })
+    // const token = req.cookies.jwt;
+    const authHeader = req.headers["authorization"]
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({message: 'Unauthorized, Bearer token missing!'})
     }
-}
+    const token = authHeader.split(' ')[1];
+    const decoded = verifyToken(token)
+    req.user = decoded.data
+    next();
+} 
+    
